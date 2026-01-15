3992a291b54202061f342478a7bb07186efbd3af
MaratMadiev <59918234+MaratMadiev@users.noreply.github.com>
2026-01-15 13:34:52 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './i18n/index';
+import './App.css';
+import Notifications from './components/Notifications';
+
+function App() {
+  return (
+    <div className="App">
+      <Notifications></Notifications>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Notifications.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { useTranslation } from "react-i18next";
+
+const Notifications: React.FC = () => {
+    let msgCount = Math.floor(Math.random() * 10) + 1
+    const now: Date = new Date();
+    const {t} = useTranslation();
+
+    const formShortDate = (date: Date): string => {
+        return new Intl.DateTimeFormat('ru-RU', {
+            day: '2-digit',
+            month: 'short',
+            hour: '2-digit',
+            minute: '2-digit',
+            second: '2-digit',
+            hour12: false
+        }).format(date)
+    }
+
+    return (
+        <div className="notification-card">
+            <div className="notification-content">
+                <div className="message">
+                    <p className="msg-icon">📩</p>
+                    <p className="msg-text">{t('unread_messages', { count: msgCount })}</p>
+                    <p className="msg-last-date">(последнее: {formShortDate(now)})</p>
+                </div>
+            </div>
+       </div>
+    )
+}
+
+export default Notifications;
